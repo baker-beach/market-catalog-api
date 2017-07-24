64cb07fbedd20f5b13c3a982d913b539db4b1d2b
achimwolf <achim.wolf@baker-beach.com>
2017-07-24 15:40:28 +0200

    fix

--- src/main/java/com/bakerbeach/market/xcatalog/model/SearchResult.java
+++ src/main/java/com/bakerbeach/market/xcatalog/model/SearchResult.java
@@ -3,10 +3,47 @@ package com.bakerbeach.market.xcatalog.model;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import com.bakerbeach.market.xcatalog.model.Product;
+import com.bakerbeach.market.xcatalog.model.Facets;
+import com.bakerbeach.market.xcatalog.model.Group;
+import com.bakerbeach.market.xcatalog.model.Pager;
 
 public class SearchResult {
 
-	protected Collection<Product> groups = new ArrayList<>();
+	protected Collection<Group> groups = new ArrayList<>();
+	protected Facets facets;
+	protected Pager pager;
+	protected String filterUrl;
+
+	public Collection<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Collection<Group> groups) {
+		this.groups = groups;
+	}
+
+	public Facets getFacets() {
+		return facets;
+	}
+
+	public void setFacets(Facets facets) {
+		this.facets = facets;
+	}
+
+	public Pager getPager() {
+		return pager;
+	}
+
+	public void setPager(Pager pager) {
+		this.pager = pager;
+	}
+
+	public String getFilterUrl() {
+		return filterUrl;
+	}
+
+	public void setFilterUrl(String filterUrl) {
+		this.filterUrl = filterUrl;
+	}
 
 }
